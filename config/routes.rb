92dd8b1aedd0b14bf4161ffc399a52a343415f47
Ross Keenan <ross.keenan@protonmail.com>
2021-08-22 09:37:49 -0500

    Added in basic sign-up form with Bootstrap styling. Added sign up page to the navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   #Creating alias for for about route
   get "about", to: "about#index", as: :about
 
-
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 
   #This is a helper that Rails has, so you do not need to do "get '/'"
   root to: "main#index"
