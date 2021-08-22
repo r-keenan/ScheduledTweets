c10641b2d449138288eff2d2fc9a714f6f3121ad
Ross Keenan <ross.keenan@protonmail.com>
2021-08-22 06:35:17 -0500

    Added alias to about route. implemented Bootstrap navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get "about", to: "about#index"
+  #Creating alias for for about route
+  get "about", to: "about#index", as: :about
 
-  
+
 
   #This is a helper that Rails has, so you do not need to do "get '/'"
   root to: "main#index"
