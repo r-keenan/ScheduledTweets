5c474bde922a1e0df4aefffd78bda813d17ccb79
Ross Keenan <ross.keenan@protonmail.com>
2021-08-22 06:07:23 -0500

    Added action to root. Added in Bootstrap CDN to layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   get "about", to: "about#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  
+
+  #This is a helper that Rails has, so you do not need to do "get '/'"
+  root to: "main#index"
+  
 end
