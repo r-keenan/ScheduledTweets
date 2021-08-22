c0062b2d3ea93eba2bec1c55706156e7b9b29cbe
Ross Keenan <ross.keenan@protonmail.com>
2021-08-22 06:54:36 -0500

    created flash partial. created new methods for flashes. added partial to layout.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,13 @@
 class MainController < ApplicationController
     def index
     end
+
+    def notice_flash
+        flash[:notice] = "Logged in successfully"
+    end
+
+    def alert_flash
+        flash[:alert] = "Invalid email or password"
+    end
+
 end
